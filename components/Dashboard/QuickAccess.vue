551cda22190c6767ed212064f38d47a032d6f25c
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2023-01-17 21:00:18 +0300

    add

---
+++ components/Dashboard/QuickAccess.vue
@@ -0,0 +1,10 @@
+<template>
+    <ABtn v-for="item in dashboard.tabs" :key="item" @click="dashboard.selecteTab(item)" h="70px" text-2xl text-tertiary>
+        {{ item }}
+    </ABtn>
+</template>
+            
+<script setup>
+const dashboard = useDashboard();
+console.log(dashboard.selected);
+</script>
---
+++ components/Products/NewProducte.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>New Product</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
